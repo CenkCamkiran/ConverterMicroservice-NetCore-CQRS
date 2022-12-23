5eedb31e44293944bb8d2eabb23c5bfd09039249
Cenk Camkıran <34924560+CenkCamkiran@users.noreply.github.com>
2022-12-23 17:58:29 +0300

    date added to converter log

--- LoggerMicroservice/Models/ConverterLog.cs
+++ LoggerMicroservice/Models/ConverterLog.cs
@@ -1,8 +1,11 @@
-﻿namespace Models
+﻿using System;
+
+namespace Models
 {
     public class ConverterLog
     {
         public string Error { get; set; } = string.Empty;
         public string Info { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
     }
 }
