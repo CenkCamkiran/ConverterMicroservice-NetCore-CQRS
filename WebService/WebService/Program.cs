c78bbb337fa0018fed1fac65f40714a29f06d8ef
Cenk Camkıran <34924560+CenkCamkiran@users.noreply.github.com>
2023-02-26 23:53:24 +0300

    mediatr fixed

--- WebService/WebService/Program.cs
+++ WebService/WebService/Program.cs
@@ -1,3 +1,14 @@
+using Elasticsearch.Net;
+using Minio;
+using Nest;
+using RabbitMQ.Client;
+using WebService.Commands.LogCommands;
+using WebService.Commands.ObjectCommands;
+using WebService.Commands.QueueCommands;
+using WebService.Handlers.HealthHandlers;
+using WebService.Handlers.LogHandlers;
+using WebService.Handlers.ObjectHandlers;
+using WebService.Handlers.QueueHandlers;
 using WebService.Helpers.Helpers;
 using WebService.Helpers.Interfaces;
 using WebService.Middlewares;
@@ -5,6 +16,7 @@ using WebService.Middlewares.Contexts;
 using WebService.Middlewares.Contexts.Interfaces;
 using WebService.Models;
 using WebService.ProjectConfigurations;
+using WebService.Queries.HealthQueries;
 using WebService.Repositories.Interfaces;
 using WebService.Repositories.Repositories;
 
@@ -44,41 +56,32 @@ builder.Services.AddScoped<IPingHelper, PingHelper>();
 //Context
 builder.Services.AddScoped<IWebServiceContext, WebServiceContext>();
 
-//ConnectionSettings? connection = new ConnectionSettings(new Uri(elkEnvVariables.ElkHost)).
-//   DefaultIndex(elkEnvVariables.ElkDefaultIndex).
-//   ServerCertificateValidationCallback(CertificateValidations.AllowAll).
-//   ThrowExceptions(true).
-//   PrettyJson().
-//   RequestTimeout(TimeSpan.FromSeconds(300)).
-//   BasicAuthentication(elkEnvVariables.ElkUsername, elkEnvVariables.ElkPassword); //.ApiKeyAuthentication("<id>", "<api key>"); 
+ConnectionSettings? connection = new ConnectionSettings(new Uri(elkEnvVariables.ElkHost)).
+   DefaultIndex(elkEnvVariables.ElkDefaultIndex).
+   ServerCertificateValidationCallback(CertificateValidations.AllowAll).
+   ThrowExceptions(true).
+   PrettyJson().
+   RequestTimeout(TimeSpan.FromSeconds(300)).
+   BasicAuthentication(elkEnvVariables.ElkUsername, elkEnvVariables.ElkPassword); //.ApiKeyAuthentication("<id>", "<api key>"); 
 
-//ElasticClient? elasticClient = new ElasticClient(connection);
-//builder.Services.AddSingleton<IElasticClient>(elasticClient);
+ElasticClient? elasticClient = new ElasticClient(connection);
+builder.Services.AddSingleton<IElasticClient>(elasticClient);
 
-//var connectionFactory = new ConnectionFactory
-//{
-//    HostName = rabbitEnvVariables.RabbitMqHost,
-//    Port = Convert.ToInt32(rabbitEnvVariables.RabbitMqPort),
-//    UserName = rabbitEnvVariables.RabbitMqUsername,
-//    Password = rabbitEnvVariables.RabbitMqPassword
-//};
-//var rabbitConnection = connectionFactory.CreateConnection();
-//builder.Services.AddSingleton<IConnection>(rabbitConnection);
+var connectionFactory = new ConnectionFactory
+{
+    HostName = rabbitEnvVariables.RabbitMqHost,
+    Port = Convert.ToInt32(rabbitEnvVariables.RabbitMqPort),
+    UserName = rabbitEnvVariables.RabbitMqUsername,
+    Password = rabbitEnvVariables.RabbitMqPassword
+};
+var rabbitConnection = connectionFactory.CreateConnection();
+builder.Services.AddSingleton<IConnection>(rabbitConnection);
 
-//MinioClient minioClient = new MinioClient()
-//                                    .WithEndpoint(minioEnvVariables.MinioHost)
-//                                    .WithCredentials(minioEnvVariables.MinioAccessKey, minioEnvVariables.MinioSecretKey)
-//                                    .WithSSL(false);
-//builder.Services.AddSingleton<IMinioClient>(minioClient);
-
-//Assembly.GetAssembly(typeof(LogCommand));
-//Assembly.GetAssembly(typeof(ObjectCommand));
-//Assembly.GetAssembly(typeof(QueueCommand));
-//Assembly.GetAssembly(typeof(LogHandler));
-//Assembly.GetAssembly(typeof(ObjectHandler));
-//Assembly.GetAssembly(typeof(HealthHandler));
-//Assembly.GetAssembly(typeof(QueueHandler));
-//Assembly.GetAssembly(typeof(HealthQuery));
+MinioClient minioClient = new MinioClient()
+                                    .WithEndpoint(minioEnvVariables.MinioHost)
+                                    .WithCredentials(minioEnvVariables.MinioAccessKey, minioEnvVariables.MinioSecretKey)
+                                    .WithSSL(false);
+builder.Services.AddSingleton<IMinioClient>(minioClient);
 
 //new MediatRServiceConfiguration().RegisterServicesFromAssembly()
 //builder.Services.AddMediatR(typeof(Program));
@@ -88,11 +91,19 @@ builder.Services.AddScoped<IWebServiceContext, WebServiceContext>();
 
 builder.Services.AddMediatR((MediatRServiceConfiguration configuration) =>
 {
-    var Handlers = AppDomain.CurrentDomain.Load("WebService.Handlers");
-    var Queries = AppDomain.CurrentDomain.Load("WebService.Queries");
-    var Commands = AppDomain.CurrentDomain.Load("WebService.Commands");
+    //var Handlers = AppDomain.CurrentDomain.Load("WebService.Handlers");
+    //var Queries = AppDomain.CurrentDomain.Load("WebService.Queries");
+    //var Commands = AppDomain.CurrentDomain.Load("WebService.Commands");
 
-    configuration.RegisterServicesFromAssemblies(Handlers, Queries, Commands);
+    configuration.RegisterServicesFromAssemblies(
+        typeof(LogCommand).Assembly, 
+        typeof(ObjectCommand).Assembly, 
+        typeof(QueueCommand).Assembly, 
+        typeof(LogHandler).Assembly,
+        typeof(ObjectHandler).Assembly,
+        typeof(HealthHandler).Assembly,
+        typeof(QueueHandler).Assembly,
+        typeof(HealthQuery).Assembly);
 });
 
 builder.Services.AddEndpointsApiExplorer();
